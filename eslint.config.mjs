54c26cc1688e2dc87acb9ca0490101edd4b27a4a
aburahan8721 <aburaihan8721@gmail.com>
2024-07-09 06:49:43 +0600

    some changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,19 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: { globals: globals.browser },
+    ignores: ['**/node_modules/', '.dist/'],
+
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
