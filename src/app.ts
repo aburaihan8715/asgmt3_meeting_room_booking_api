90f9f17d769069f844476878b7ef474e78f8b12d
aburahan8721 <aburaihan8721@gmail.com>
2024-09-10 10:22:12 +0600

    some change

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      'http://localhost:5173',
       'https://meeting-room-booking-sepia.vercel.app',
+      'http://localhost:5173',
     ],
     credentials: true,
   }),
