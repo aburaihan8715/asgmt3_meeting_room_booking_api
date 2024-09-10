938e6e1a244da08ed3443c5f181f21faa53c2499
aburahan8721 <aburaihan8721@gmail.com>
2024-09-10 08:07:15 +0600

    implement cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,15 @@ const app = express();
 // GLOBAL MIDDLEWARES
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://meeting-room-booking-sepia.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 
 // HOME ROUTE
 app.get('/', (req: Request, res: Response) => {
